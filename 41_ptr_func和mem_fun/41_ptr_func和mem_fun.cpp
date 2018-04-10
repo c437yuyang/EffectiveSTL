4eb7c8ad14cfc89fd16c2912c773289bb899ee25
c437yuyang <954222619@qq.com>
2018-04-10 21:41:15 +0800

    review complete

--- 41_ptr_func和mem_fun/41_ptr_func和mem_fun.cpp
+++ 41_ptr_func和mem_fun/41_ptr_func和mem_fun.cpp
@@ -14,6 +14,7 @@ class Widget {
 public:
 	Widget() { m_wId++; m_Id = m_wId; }
 	void printInfo() { cout << m_Id << endl; }
+	void printInfo1(int ii) { cout << m_Id << ii << endl; }
 private:
 	int m_Id;
 	static int m_wId;
@@ -29,13 +30,14 @@ int main()
 	for_each(v1.begin(), v1.end(), test); //#1 非成员函数的调用方式
 	for_each(v1.begin(), v1.end(), ptr_fun(test)); //#1 非成员函数的调用方式,加上ptr_fun也可以，多一些typedef是不会错的
 	for_each(v1.begin(), v1.end(), mem_fun_ref(&Widget::printInfo)); //#3 成员函数,传递对象本身时用mem_fun_ref来适配成为mem_fun_ref_t对象
+	//for_each(v1.begin(), v1.end(), mem_fun_ref(&Widget::printInfo1)); //不能传递参数，所以真的不好用,不如用lambda表达式
 
 	vector<Widget*> pv1 = { new Widget(),new Widget(),new Widget(),new Widget(),new Widget() };
 	//for_each(pv1.begin(), pv1.end(), &Widget::printInfo); //#2 报错，不能直接传递成员函数
 	for_each(pv1.begin(), pv1.end(), mem_fun(&Widget::printInfo)); //#2 成员函数,传递指针的时候用mem_fun适配成mem_fun_t对象
 
 
-	//lambda表达式都可以很容易的解决
+	//lambda表达式都可以很容易的解决,而且mem_fun不能传递参数
 	for_each(v1.begin(), v1.end(), [](Widget &w) {w.printInfo(); });
 	for_each(pv1.begin(), pv1.end(), [](Widget *pw) {pw->printInfo(); });
 
